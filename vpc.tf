a3483a6e0e7b7881fd7396c7b9c73507cd3179d3
ShantanuKokate <83412486+ShantanuKokate@users.noreply.github.com>
2025-04-06 17:19:06 +0530

    Add files via upload
    vpc configurations

---
+++ vpc.tf
@@ -0,0 +1,20 @@
+# vpc.tf
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-southeast-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-southeast-1b"
+  map_public_ip_on_launch = true
+}
+
